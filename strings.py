36b006fc15cd1c176217af19dd48c19c83ed088a
manikanta611 <74701882+manikanta611@users.noreply.github.com>
2020-12-14 09:28:09 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,21 @@
+msg1 = 'HOOLAA'
+print(msg1)
+
+m2 = 'He said,\'let us python\''
+print(m2)
+
+m4 = 'C:\\temp\\new file'
+print(m4)
+
+m5 = r'C:\temp\new file'
+print(m5)
+
+m6 = ('What is this life if full of care...'
+  'We have no time to stand and stare')
+print(m6)
+
+m7 = 'utopia'
+m8 = 'today!!!'
+m7 = m7 + str(' ') + m8
+print(m7)
+print(len(m7))
